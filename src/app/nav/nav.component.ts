7839cdae37bfb51ae288eddbf5904668f7dadd94
Strahinja <standaard@iMac-3.local>
2019-01-16 13:59:38 +0100

    Made some changes to the styling of the text boxes and the inputs

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   appTitle: string = 'My Todo App';
+  logoImg = "https://vignette.wikia.nocookie.net/logopedia/images/f/f0/Washington_capitals_alternate_logo.gif/revision/latest?cb=20130929140614";
 
   constructor() { }
 
